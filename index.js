14da2dc45b947dfc722c6d99537e4e10f1ec1468
sadhin28 <taosifsadhin113@gmail.com>
2025-06-17 21:25:33 +0600

    add user data in fetch

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app= express();
 const port = process.env.PORT || 5000;
 
+
+
 const users=[
     {id:1,name:"Sabana",email:'sabana@gmail.com'},
     {id:2,name:"Sabnur",email:'sabnur@gmail.com'},

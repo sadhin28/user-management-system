d773391cca1b1303378faf6b7e1677b5398e4ddf
sadhin28 <taosifsadhin113@gmail.com>
2025-06-17 21:13:38 +0600

    create a server name user

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 const express = require('express');
 const app= express();
+const port = process.env.PORT || 5000;
+
+const users=[
+    {id:1,name:"Sabana",email:'sabana@gmail.com'},
+    {id:2,name:"Sabnur",email:'sabnur@gmail.com'},
+    {id:3,name:"Sabila",email:'sabila@gmail.com'},
+]
+
+app.get('/',(req,res)=>{
+    res.send('User Management server is running')
+})
+
+app.get('/users',(req,res)=>{
+    res.send(users)
+})
+app.listen(port,(req,res)=>{
+    console.log(`Server is running on PORT`,port)
+})

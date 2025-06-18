5278245809f650124ec4cc8922bec677eb0bacc0
sadhin28 <taosifsadhin113@gmail.com>
2025-06-18 22:01:39 +0600

    add async function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app= express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors())
 app.use(express.json())
 
@@ -11,6 +12,33 @@ const users=[
     {id:3,name:"Sabila",email:'sabila@gmail.com'},
 ]
 
+//pass: PUJcZgkHBvUHaL7C
+//username: taosifsadhin113
+//mongodb code start
+
+const uri = "mongodb+srv://taosifsadhin113:PUJcZgkHBvUHaL7C@sadhin.ix1e55h.mongodb.net/?retryWrites=true&w=majority&appName=sadhin";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+//mongodb code end
 app.get('/',(req,res)=>{
     res.send('User Management server is running')
 })

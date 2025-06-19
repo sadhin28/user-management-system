339980cc8e3719e392d66a5668e58de6432279fb
sadhin28 <taosifsadhin113@gmail.com>
2025-06-20 00:47:45 +0600

    done all work

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app= express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors())
 app.use(express.json())
 
@@ -53,9 +53,12 @@ async function run() {
     // console.log(req.body)
     
 });
-app.delete('/user/:id',(req,res)=>{
+app.delete('/user/:id',async (req,res)=>{
   const id =req.params.id;
   console.log('plase delate from database',id)
+  const query =  {_id: new ObjectId( id)}
+  const result=await userCollection.deleteOne(query)
+  res.send(result)
 })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
